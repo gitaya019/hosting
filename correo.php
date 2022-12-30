1f98472943193a3dd27c35128f3bf46a631ec769
gitaya019 <113080923+gitaya019@users.noreply.github.com>
2022-12-30 13:09:59 -0500

    Update correo.php

--- correo.php
+++ correo.php
@@ -1,5 +1,5 @@
 <?php
-    $destinatario = 'ayalajhoan24@gmail.com'
+    $destinatario = 'ayalajhoan24@gmail.com';
     $nombre = $_POST['nombre'];
     $asunto =$_POST['asunto'];
     $mensaje =$_POST['mensaje'];
@@ -9,7 +9,7 @@
     $mensajeCompleto = $mensaje . "\nAtentamente " .$nombre;
 
     mail($destinatario, $asunto, $mensajeCompleto, $header);
-    echo "<script>alert('correo enviado exitosamente')</script>"
-    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>"
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
 
 ?>
